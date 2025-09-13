aa071d85a834822dbd50c6e646f8ab7f837a6935
ropl.btc <github.com.thermal146@passmail.net>
2025-09-13 22:35:27 +0200

    fix: cloudflare...

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare/dev";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default initOpenNextCloudflareForDev(nextConfig);
